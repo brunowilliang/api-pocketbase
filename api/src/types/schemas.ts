b16093d798e4b3c26e7d04bdcce25833bd133239
Bruno Garcia <brunowilliang@icloud.com>
2025-05-30 15:15:41 +0100

    test migrations

---
+++ api/src/types/schemas.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod'
+
+const DATETIME_REGEX = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d+)?Z$/
+
+export const usersSchema = z.object({
+    id: z.string().regex(/^[a-z0-9]+$/).length(15).optional(),
+    password: z.string().min(8),
+    tokenKey: z.string().min(30).max(60).optional(),
+    email: z.string().email(),
+    emailVisibility: z.boolean().optional(),
+    verified: z.boolean().optional(),
+    name: z.string().max(255).optional(),
+    surname: z.string().optional(),
+    avatar: z.string().optional(),
+    rule: z.enum(["test 1", "test 2", "test 3"]).optional(),
+    created: z.string().regex(DATETIME_REGEX).optional(),
+    updated: z.string().regex(DATETIME_REGEX).optional(),
+})
+
---
+++ api/src/types/types.ts
@@ -0,0 +1,30 @@
+export interface Users {
+    id: string
+    password: string
+    tokenKey: string
+    email: string
+    emailVisibility: boolean
+    verified: boolean
+    name: string
+    surname: string
+    avatar: string
+    rule: 'test 1' | 'test 2' | 'test 3'
+    created: string
+    updated: string
+}
+
+
+/**
+ * Commented-out back-relations are what will be inferred by pocketbase-ts from the forward relations.
+ *
+ * The "UNIQUE index constraint" case is automatically handled by this hook,
+ * but if you want to make a back-relation non-nullable, you can uncomment it and remove the "?".
+ *
+ * See [here](https://github.com/satohshi/pocketbase-ts#back-relations) for more information.
+ */
+export type Schema = {
+    users: {
+        type: Users
+    }
+}
+
